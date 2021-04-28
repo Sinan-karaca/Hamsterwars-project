b59b44f0cb2716bedbdb5924b2b007414246c728
Sinan-karaca <70816608+Sinan-karaca@users.noreply.github.com>
2021-04-28 13:00:45 +0200

    fixed the PUT problem for the test and it is working now

--- hamsterValidation.js
+++ hamsterValidation.js
@@ -1,68 +1,45 @@
 function postValidHamsterObj(isObj) {
-  let error = "";
-  if (!isObj.name) {
-    error += "Parameter name cannot be null or empty string. \n";
-  } else if (typeof isObj.name !== "string") {
-    error += "Parameter name must be a string. \n";
-  } else if (isObj.name === "") {
-    error += "Parameter name cannot be an empty string. \n ";
-  }
+    let error = "";
+    if (!isObj.name) {
+      error += "Parameter name cannot be null or empty string. \n";
+    } else if (typeof isObj.name !== "string") {
+      error += "Parameter name must be a string. \n";
+    } else if (isObj.name === "") {
+      error += "Parameter name cannot be an empty string. \n ";
+    }
 
-  if (!isObj.age) {
-    error += "Parameter age cannot be null or an empty field. \n";
-  } else if (typeof isObj.age !== "number") {
-    error += "Parameter age must be a number. \n";
-  } else if (isObj.age === "") {
-    error += "Parameter age cannot be an empty field. \n";
-  }
+    if (!isObj.age) {
+      error += "Parameter age cannot be null or an empty field. \n";
+    } else if (typeof isObj.age !== "number") {
+      error += "Parameter age must be a number. \n";
+    } else if (isObj.age === "") {
+      error += "Parameter age cannot be an empty field. \n";
+    }
 
-  if (!isObj.favFood) {
-    error += "Parameter favFood cannot be null or an empty string. \n";
-  } else if (typeof isObj.favFood !== "string") {
-    error += "Parameter favFood must be a string. \n";
-  } else if (isObj.favFood === "") {
-    error += "Parameter favFood cannot be an empty string. \n";
-  }
+    if (!isObj.favFood) {
+      error += "Parameter favFood cannot be null or an empty string. \n";
+    } else if (typeof isObj.favFood !== "string") {
+      error += "Parameter favFood must be a string. \n";
+    } else if (isObj.favFood === "") {
+      error += "Parameter favFood cannot be an empty string. \n";
+    }
 
-  if (!isObj.loves) {
-    error += "Parameter loves cannot be null or an empty string. \n";
-  } else if (typeof isObj.loves !== "string") {
-    error += "Parameter loves must be a string. \n";
-  } else if (isObj.loves === "") {
-    error += "Parameter loves cannot be an empty string. \n";
-  }
+    if (!isObj.loves) {
+      error += "Parameter loves cannot be null or an empty string. \n";
+    } else if (typeof isObj.loves !== "string") {
+      error += "Parameter loves must be a string. \n";
+    } else if (isObj.loves === "") {
+      error += "Parameter loves cannot be an empty string. \n";
+    }
 
-  if (!isObj.imgName) {
-    error += "Parameter imgName cannot be null or an empty string. \n";
-  } else if (typeof isObj.imgName !== "string") {
-    error += "Parameter imgName must be a string. \n";
-  } else if (isObj.imgName === "") {
-    error += "Parameter imgName cannot be an empty string. \n";
-  }
+    if (!isObj.imgName) {
+      error += "Parameter imgName cannot be null or an empty string. \n";
+    } else if (typeof isObj.imgName !== "string") {
+      error += "Parameter imgName must be a string. \n";
+    } else if (isObj.imgName === "") {
+      error += "Parameter imgName cannot be an empty string. \n";
+    }
 
-//   if (!isObj.wins) {
-//     error += "Parameter wins cannot be null or an empty field. \n";
-//   } else if (typeof isObj.wins !== "number") {
-//     error += "Parameter wins must be a number. \n";
-//   } else if (isObj.wins === "") {
-//     error += "Parameter wins cannot be an empty field. \n";
-//   }
-
-//   if (!isObj.defeats) {
-//     error += "Parameter defeats cannot be null or an empty field. \n";
-//   } else if (typeof isObj.defeats !== "number") {
-//     error += "Parameter defeats must be a number. \n";
-//   } else if (isObj.defeats === "") {
-//     error += "Parameter defeats cannot be an empty field. \n";
-//   }
-
-//   if (!isObj.games) {
-//     error += "Parameter games cannot be null or an empty field. \n";
-//   } else if (typeof isObj.games !== "number") {
-//     error += "Parameter games must be a number. \n";
-//   } else if (isObj.games === "") {
-//     error += "Parameter games cannot be an empty field. \n";
-//   }
   return error;
 }
 function putValidHamsterObj(isObj) {
--- routes/hamsters.js
+++ routes/hamsters.js
@@ -1,151 +1,137 @@
 const getDatabase = require("../database.js");
 const firebase = getDatabase();
-
 const hamsterValidation = require("../hamsterValidation.js");
-const mapper = require("../hamsterMapper");
-
 const express = require("express");
-const { json } = require("express");
 const router = express.Router();
 
-// *** REST API ***
-
-// GET HAMSTERS
-
-// router.get('/', (req,res) => {
-// 	console.log('/hamsters Rest Api');
-// 	res.send('Welcome to the Hamsterwar project')
-
-// })
-
 router.get("/", async (req, res) => {
-  console.log("/hamsters Rest Api");
+    console.log("/hamsters Rest Api");
 
-  const hamstersRef = firebase.collection("hamsters");
-  const snapshot = await hamstersRef.get();
+    const hamstersRef = firebase.collection("hamsters");
+    const snapshot = await hamstersRef.get();
 
-  if (snapshot.empty) {
-    res.send([]);
-    return;
-  }
+    if (snapshot.empty) {
+      res.send([]);
+      return;
+    }
 
-  let items = [];
-  snapshot.forEach((doc) => {
-    const data = doc.data();
-    data.id = doc.id; // id behövs för POST+PUT+DELETE
-    items.push(data);
-  });
-  res.send(items);
+    let items = [];
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+      data.id = doc.id; // id behövs för POST+PUT+DELETE
+      items.push(data);
+    });
+    res.send(items);
 });
 
 router.get("/random", async (req, res) => {
-  console.log("/hamsters Rest Api");
-  const hamstersRef = firebase.collection("hamsters");
-  const snapshot = await hamstersRef.get();
+    console.log("/hamsters Rest Api");
+    const hamstersRef = firebase.collection("hamsters");
+    const snapshot = await hamstersRef.get();
 
-  if (snapshot.empty) {
-    res.status(404).send("No hamster found.");
-    return;
-  }
-
-  var i = 0;
-  var rand = Math.floor(Math.random() * snapshot.size);
-  snapshot.forEach((doc) => {
-    if (i == rand) {
-      const data = doc.data();
-      data.id = doc.id; // id behövs för POST+PUT+DELETE
-      res.send(data);
+    if (snapshot.empty) {
+      res.status(404).send("No hamster found.");
       return;
     }
-    i++;
-  });
+
+    var i = 0;
+    var rand = Math.floor(Math.random() * snapshot.size);
+    snapshot.forEach((doc) => {
+      if (i == rand) {
+        const data = doc.data();
+        data.id = doc.id; // id behövs för POST+PUT+DELETE
+        res.send(data);
+        return;
+      }
+      i++;
+    });
 });
 
 router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const hamstersRef = await firebase.collection("hamsters").doc(id).get();
+    const id = req.params.id;
+    const hamstersRef = await firebase.collection("hamsters").doc(id).get();
 
-  if (!hamstersRef.exists) {
-    res.status(404).send("Hamster does not exist");
-    return;
-  }
+    if (!hamstersRef.exists) {
+      res.status(404).send("Hamster does not exist");
+      return;
+    }
 
-  const data = hamstersRef.data();
-  res.status(200).send(data);
+    const data = hamstersRef.data();
+    res.status(200).send(data);
 });
 
 router.post("/", async (req, res) => {
-  // OBS! Måste installera express.json för att detta ska fungera
-  const object = req.body;
-  let valid = hamsterValidation.postValidHamsterObj(object);
-  if (valid !== "") {
-    res.status(400).send(valid);
-    return;
-  }
-  const docRef = await firebase.collection("hamsters").add(object);
-  return res.status(200).send({ id: docRef.id });
+    // OBS! Måste installera express.json för att detta ska fungera
+    const object = req.body;
+    let valid = hamsterValidation.postValidHamsterObj(object);
+    if (valid !== "") {
+      res.status(400).send(valid);
+      return;
+    }
+    const docRef = await firebase.collection("hamsters").add(object);
+    return res.status(200).send({ id: docRef.id });
 });
 
 router.put("/:id", async (req, res) => {
-  const object = req.body;
-  const id = req.params.id;
-  if (id === undefined) {
-    res.sendStatus(400);
-    return;
-  }
+    const object = req.body;
+    const id = req.params.id;
+    if (!id ||!object || (Object.keys(object).length === 0 && object.constructor === Object)) {
+      res.sendStatus(400);
+      return;
+    }
 
-  let valid = hamsterValidation.putValidHamsterObj(object);
-  if (valid !== "") {
-    res.status(400).send(valid);
-    return;
-  }
-  const hamstersRef = await firebase.collection("hamsters").doc(id).get();
+    let valid = hamsterValidation.putValidHamsterObj(object);
+    if (valid !== "") {
+      res.status(400).send(valid);
+      return;
+    }
+    const hamstersRef = await firebase.collection("hamsters").doc(id).get();
 
-  if (!hamstersRef.exists) {
-    res.status(404).send("Hamster does not exist");
-    return;
-  }
-  try {
-    const document = firebase.collection("hamsters").doc(id);
-    document.get().then((docSnapshot) => {
-      if (!docSnapshot.exists) {
-        return res.status(404).send("does not exist");
-      }
-    });
-    await document.set(object, { merge: true });
-    return res.status(200).send();
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
-  }
+    if (!hamstersRef.exists) {
+      res.status(404).send("Hamster does not exist");
+      return;
+    }
+    try {
+      const document = firebase.collection("hamsters").doc(id);
+      document.get().then((docSnapshot) => {
+        if (!docSnapshot.exists) {
+          return res.status(404).send("does not exist");
+        }
+      });
+      await document.set(object, { merge: true });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
 });
 
 router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  if (!id) {
-    res.sendStatus(400);
-    return;
-  }
-  const hamstersRef = await firebase.collection("hamsters").doc(id).get();
+    if (!id) {
+      res.sendStatus(400);
+      return;
+    }
+    const hamstersRef = await firebase.collection("hamsters").doc(id).get();
 
-  if (!hamstersRef.exists) {
-    res.status(404).send("Hamster does not exist");
-    return;
-  }
-  const deleted = await firebase
-    .collection("hamsters")
-    .doc(id)
-    .delete()
-    .then((kalle) => {
-      console.log(kalle);
-      console.log("Document successfully deleted!");
-      return res.status(200).send();
-    })
-    .catch((error) => {
-      console.error("Error removing document: ", error);
-      return res.status(500).send(error);
-    });
+    if (!hamstersRef.exists) {
+      res.status(404).send("Hamster does not exist");
+      return;
+    }
+    const deleted = await firebase
+      .collection("hamsters")
+      .doc(id)
+      .delete()
+      .then((kalle) => {
+        console.log(kalle);
+        console.log("Document successfully deleted!");
+        return res.status(200).send();
+      })
+      .catch((error) => {
+        console.error("Error removing document: ", error);
+        return res.status(500).send(error);
+      });
 });
 
 module.exports = router;
