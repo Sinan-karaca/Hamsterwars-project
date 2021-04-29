4c6ca9f1d99ac6b476c1625d59f1adabdfc5e4d0
Sinan-karaca <70816608+Sinan-karaca@users.noreply.github.com>
2021-04-29 12:48:04 +0200

    modified database.js for heroku again

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin')
 let serviceAccount;
 if( process.env.PRIVATE_KEY ) {
 	// På Heroku
-	serviceAccount = process.env.PRIVATE_KEY
+	serviceAccount = JSON.parse(process.env.PRIVATE_KEY)
 } else {
 	// Lokalt (på min dator)
 	serviceAccount = require('./secret.json');
